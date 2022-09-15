8fdede17f4dfb748fdf2f6b45980d7ee0ebe4e25
simonklejnstrup <sklejnstrup@gmail.com>
2022-09-15 11:37:21 +0200

    Did recyclerview with dummy data on BredsListFragment

---
+++ app/src/main/java/com/example/doggypediaapp/ui/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.doggypediaapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.doggypediaapp.R
+import com.example.doggypediaapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var navHostFragment: NavHostFragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.main_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+    }
+
+
+}
